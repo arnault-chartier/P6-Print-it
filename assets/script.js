92a99890c3753fef9a18af253a93b2cca5dd17f4
Arnault Chartier <arnault.chartier@gmail.com>
2024-06-08 21:49:47 +0200

    changement des slides au clic

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,111 @@
 // Tableau des slides
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-// Event listeners sur les fleches du carrousel
-let arrowLeft = document.getElementById("arrowLeft")
-arrowLeft.addEventListener("click",() => {
-	alert("Clic gauche")
+// Variables générales
+let i = 0
+
+// Fonction initialisation bullet points
+function initBulletPoints() {
+	for (let i = 0; i < slides.length; i++) {
+		let dots = document.querySelector(".dots")
+		let dot = document.createElement("span")
+		dot.classList.add("dot")
+		if (i == 0) {
+			dot.classList.add("dot_selected")
+		}
+		dots.appendChild(dot)	
+	}
+}
+
+// Initialisation bullet points
+initBulletPoints()
+
+// Fonction initialisation slide
+function initSlide() {
+	let imagePath = slides[i].image
+	let slide = document.querySelector(".banner-img")
+	slide.setAttribute("src", "./assets/images/slideshow/" + imagePath)
+}
+
+// Fonction initialisation texte slide
+function initSlideText() {
+	let tagLine = slides[i].tagLine
+	let slideText = document.getElementById("slideText")
+	slideText.innerHTML = tagLine
+}
+
+// Fonction changement bullet point actif
+function changeActiveDot() {
+	document.querySelector(".dot_selected").classList.remove("dot_selected")
+	let dotsList = document.querySelectorAll(".dots span")
+	let activeDot = dotsList[i]
+	activeDot.classList.add("dot_selected")
+}
+
+// Fonction initialisation slideshow
+function initSlideshow() {
+	initSlide()
+	initSlideText()
+	changeActiveDot()
+}
+
+// Fonction slide suivante
+function nextSlide() {
+	i++
+	initSlideshow()
+}
+
+// Fonction slide precedente
+function previousSlide () {
+	i--
+	initSlideshow()
+}
+
+// Event listener clic fleche droite
+let arrowRight = document.getElementById("arrowRight")
+arrowRight.addEventListener("click", () => {
+	nextSlide()
 })
 
-let arrowRight = document.getElementById("arrowRight")
-arrowRight.addEventListener("click",() => {
-	alert("Clic droit")
+// Event listener clic fleche gauche
+let arrowLeft = document.getElementById("arrowLeft")
+arrowLeft.addEventListener("click", () => {
+	previousSlide()
 })
+
+
+// Tests
+
+// // Recuperation objet tableau
+// console.log(slides[0].image)
+
+// // Changement deuxieme image sur clic fleche droite
+// function deuxiemeImage() {
+// 	let slide = document.querySelector(".banner-img")
+// 	let imagePath = slides[1].image
+// 	slide.setAttribute("src", "./assets/images/slideshow/" + imagePath)
+// }
+// let arrowRight = document.getElementById("arrowRight")
+// arrowRight.addEventListener("click",() => {
+// 	deuxiemeImage()
+// })
 
 // // Creation element bullet point du carrousel
 // let dot = document.createElement("a")
@@ -38,21 +116,18 @@ arrowRight.addEventListener("click",() => {
 // let dots = document.querySelector(".dots")
 // dots.appendChild(dot)
 
-// Insertion des bullet points en fonction du nb de slides
-let dots = document.querySelector(".dots")
+// // Insertion des bullet points en fonction du nb de slides
+// let dots = document.querySelector(".dots")
 
-	// Creation premier bullet point
-let dot = document.createElement("a")
-dot.setAttribute("class", "dot dot_selected")
-dot.setAttribute("href", "#")
+// 	// Creation premier bullet point
+// let dot = document.createElement("span")
+// dot.setAttribute("class", "dot dot_selected")
+// dots.appendChild(dot)
 
-dots.appendChild(dot)
-
-	// Creation des bullet points suivants
-for (let i = 1; i < slides.length; i++) {
-	let dot = document.createElement("a")
-	dot.setAttribute("class", "dot")
-	dot.setAttribute("href", "#")
-
-	dots.appendChild(dot)
-}
+// 	// Creation des bullet points suivants
+// for (let i = 1; i < slides.length; i++) {
+// 	let dot = document.createElement("span")
+// 	dot.setAttribute("class", "dot")
+// 	dot.setAttribute("href", "#")
+// 	dots.appendChild(dot)
+// }
